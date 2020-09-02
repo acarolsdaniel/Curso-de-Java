b06c25b7622bd7d793b93619964200ba4f67290c
Ana Carol <karol.internet@hotmail.com>
2020-09-02 17:18:00 -0300

    exemplo prático de polimorfismo

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -3,37 +3,15 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 
-		// só consigo chamar o método morder se o tipo da variável for o mesmo da classe
-		// onde o método está
+		LeiteQuente leite = new LeiteQuente();
+		Preparador preparador = new Preparador();
+		preparador.prepararBebida(leite);
+		
 		System.out.println("----------------------------------------------------------------");
-		Cachorro a = new Cachorro();
-		a.falar();
-		a.morder();
-		
-		
-		//catch
-		System.out.println("----------------------------------------------------------------");
-		Animal c = new Cachorro();
-		c.falar();
-		if(c instanceof Cachorro) {
-			Cachorro b = (Cachorro)c;
-			b.morder();
-						
-		}
-		
-		Cachorro d = new Cachorro();
-		Gato g = new Gato();
-		System.out.println("----------------------------------------------------------------");
-		falar(d);
-		System.out.println("----------------------------------------------------------------");
-		falar(g);
-		
-		
+		Refrigerante refrigerante = new Refrigerante();
+		preparador.prepararBebida(refrigerante);
+	}
 	
-	}
-	//catch implicito
-	private static void falar(Animal a) {
-		a.falar();
 		
-	}
+		
 }
---
+++ src/LeiteQuente.java
@@ -0,0 +1,17 @@
+
+public class LeiteQuente extends Bebida {
+
+	public LeiteQuente() {
+		super("Leite quente",true);		
+			
+	}
+	
+	@Override
+	public void preparar() {
+		System.out.println("Pegando um copo");
+		System.out.println("Colocando leite no copo");
+	}
+	
+	
+
+}
